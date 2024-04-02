f26648655f424050458fbf550dcef71bad72b0fd
kosch104 <konstantin-schwarze@outlook.de>
2024-04-02 15:45:25 +0200

    Experimental Radio Update

--- LocalRadio.cs
+++ LocalRadio.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using Colossal.Logging;
+using Game.Audio;
 using WindowsInput;
 
 namespace RadioMediaControl;
@@ -10,7 +11,7 @@ public class LocalRadio
 {
     private static bool _stationActive = false;
     private static InputSimulator _simulator;
-    private static int currentVolume = 0;
+    private static int currentVolume = 30;
     private static readonly ILog log = LogManager.GetLogger($"{nameof(RadioMediaControl)}").SetShowsErrorsInUI(false);
 
     internal static void OnLoad()
@@ -30,31 +31,69 @@ public class LocalRadio
     {
         log.Info($"Station changed to {newStation}");
         _stationActive = newStation == "Radio Media Control";
+        if (_stationActive)
+        {
+            log.Info("Setting radio volume to 0.3");
+            AudioManager.instance.radioVolume = 0.3f;
+        }
     }
 
     private static void RadioPlayPause()
     {
-        log.Info("Hello there");
-        _simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PLAY_PAUSE);
+        if (_stationActive)
+            _simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PLAY_PAUSE);
     }
 
     private static void RadioNext()
     {
-        _simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_NEXT_TRACK);
+        if (_stationActive)
+            _simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_NEXT_TRACK);
     }
 
     private static void RadioPrevious()
     {
-        _simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PREV_TRACK);
+        if (_stationActive)
+            _simulator.Keyboard.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PREV_TRACK);
     }
 
-    private static void RadioVolumeChanged(float newVolume)
+    private static void RadioVolumeChanged(float newValue)
     {
+        int newVolume = (int) (newValue * 100);
         log.Info($"Volume changed to {newVolume}");
         if (_stationActive)
         {
-            log.Info("Station active");
-            SetVolumeTo((int)(newVolume * 100));
+            if (newVolume == 0)
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    VolumeDown();
+                }
+            }
+            if (newVolume == 100)
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    VolumeUp();
+                }
+            }
+
+            int steps = Math.Abs(newVolume - currentVolume) / 2;
+            log.Info("Changing from " + currentVolume + " to " + newVolume + " in " + steps + " steps");
+            if (newVolume > currentVolume)
+            {
+                for (int i = 0; i < steps; i++)
+                {
+                    VolumeUp();
+                }
+            }
+            else if (newVolume < currentVolume)
+            {
+                for (int i = 0; i < steps; i++)
+                {
+                    VolumeDown();
+                }
+            }
+            currentVolume = newVolume;
         }
     }
 
@@ -66,36 +105,13 @@ public class LocalRadio
     [DllImport("user32.dll")]
     public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
 
-    private static void SetVolumeTo(int newVolume)
-    {
-        if (newVolume > currentVolume)
-        {
-            log.Info("New volume is higher than current volume");
-            for (int i = currentVolume; i < newVolume; i++)
-            {
-                VolumeUp();
-            }
-        }
-        else
-        {
-            log.Info("New volume is lower than current volume");
-            for (int i = currentVolume; i > newVolume; i--)
-            {
-                VolumeDown();
-            }
-        }
-        currentVolume = newVolume;
-    }
-
     private static void VolumeUp()
     {
-        log.Info("Volume up");
         SendMessageW(Process.GetCurrentProcess().MainWindowHandle, WM_APPCOMMAND, Process.GetCurrentProcess().MainWindowHandle, (IntPtr)APPCOMMAND_VOLUME_UP);
     }
 
     private static void VolumeDown()
     {
-        log.Info("Volume down");
         SendMessageW(Process.GetCurrentProcess().MainWindowHandle, WM_APPCOMMAND, Process.GetCurrentProcess().MainWindowHandle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
     }
 }
